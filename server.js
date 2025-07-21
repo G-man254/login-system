484fa697927df72791efb048df618225b9af1adc
DENNIS <denniskarikiu@gmail.com>
2025-07-21 14:50:19 +0300

    server code commenting

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const flash = require("express-flash")
 const session = require("express-session")
 const methodOverride = require("method-override")
 
+//configuring passport for login credentials
 const initializePassport = require("./passport-config")
 initializePassport(
     passport,
@@ -21,6 +22,7 @@ initializePassport(
     }
 )
 
+//temporary storage
 const users = [];
 
 app.set("view-engine", "ejs")
@@ -36,27 +38,30 @@ app.use(passport.session())
 app.use(methodOverride("_method"))
 app.use(express.static("public"))
 
+//home route
 app.get("/", checkAuthenticated, (req, res) => {
     res.render("index.ejs", {name: req.user.name})
 })
-
+//login route
 app.get("/login", checkNotAuthenticated, (req, res) => {
     res.render("login.ejs")
 })
 
+//handles redirection of client to home page and restrains from login access once logged in
 app.post("/login", checkNotAuthenticated, passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/login",
     failureFlash: true
 }))
 
-
+//register route
 app.get("/register", checkNotAuthenticated, (req, res) => {
     res.render("register.ejs")
 })
 
 app.post("/register", checkNotAuthenticated, async(req,res) => {
     try {
+        //hashing password created by user
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         users.push({
             id: Date.now().toString(),
@@ -70,7 +75,7 @@ app.post("/register", checkNotAuthenticated, async(req,res) => {
     }
     console.log(users)
 })
-
+//logout route using methodOverride
 app.delete("/logout", (req, res, next) => {
     req.logOut(function(error){
         if(error){
@@ -80,6 +85,7 @@ app.delete("/logout", (req, res, next) => {
     });
 })
 
+//checks if user is logged in and allows to access the home route
 function checkAuthenticated(req, res, next){
     if(req.isAuthenticated()){
         return next();
@@ -87,6 +93,7 @@ function checkAuthenticated(req, res, next){
     res.redirect("/login");
 }
 
+//checks if user is logged in and denies access to the login/register routes
 function checkNotAuthenticated(req, res, next){
     if(req.isAuthenticated()){
         return res.redirect("/");
